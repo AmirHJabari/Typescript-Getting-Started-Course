ca2b71ba0f491e478e33714b05657945f79351d9
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-12-25 11:04:16 +0330

    Get advantage of type declaration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,14 @@
 function startGame() {
     // Start the game
+    const playerName: string = 'Amir';
+    logPlayer(playerName)
+
     let messagesElement = document.getElementById('messages')
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'
 }
+
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame)
