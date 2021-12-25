9623f1d5cc1c71679ee4498a76e61262872f9c41
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-12-25 09:46:48 +0330

    Init app.

---
+++ src/app/app.ts
@@ -0,0 +1,6 @@
+function startGame() {
+    // Start the game
+    let messagesElement = document.getElementById('messages')
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'
+}
+document.getElementById('startGame')!.addEventListener('click', startGame)
