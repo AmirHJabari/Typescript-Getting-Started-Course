25256edf4d5b22776d2caf7b4841f71ecee935d4
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-12-25 12:12:28 +0330

    Write better functions.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,30 @@
 function startGame() {
     // Start the game
-    let playerName: string = 'Amir';
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName)
 
     let messagesElement = document.getElementById('messages')
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'
+
+    postScore(100, playerName);
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(id: string): string | undefined {
+    const inputElemnt: HTMLInputElement = document.getElementById(id) as HTMLInputElement;
+
+    if (inputElemnt == undefined || inputElemnt.value === '')
+        return undefined;
+    else
+        return inputElemnt.value;
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    const scoreElement = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame)
